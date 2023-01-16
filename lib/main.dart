c69cacdd5ba440768b034040386e3cc7e98ef881
unknown <tritechuzaifa@gmail.com>
2023-01-16 11:24:45 +0500

    ALi

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: Styles.lightTheme,
       title: "Klicks",
-      initialRoute: 'checkOut',
+      initialRoute: 'top_up',
       routes: {
         'splash': (context) => const SplashScreen(),
         'login': (context) => const LoginScreen(),
--- lib/screen/top_up/top_up.dart
+++ lib/screen/top_up/top_up.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:klicks_app/static/box.dart';
 import 'package:klicks_app/static/title_topbar.dart';
 
 class TopUp extends StatefulWidget {
@@ -11,45 +12,99 @@ class TopUp extends StatefulWidget {
 }
 
 class _TopUpState extends State<TopUp> {
+  String? Selectedvalue = "AED 10";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          TitleTopbar(
-            text: 'Top Up',
-            ontap: () {},
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50, left: 20),
-            child: Text(
-              'Enter the amount of top up',
-              style: TextStyle(fontSize: 15),
+          child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          children: [
+            TitleTopbar(
+              text: 'Top Up',
+              ontap: () {},
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, right: 20, left: 20),
-            child: Container(
-                height: 105,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  color: Colors.white,
-                  border: Border.all(width: 1, color: Colors.black),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'AED 10',
-                      style:
-                          TextStyle(fontSize: 45, fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                )),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+              ),
+              child: Text(
+                'Enter the amount of top up',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10,
+              ),
+              child: Container(
+                  height: 105,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white,
+                    border: Border.all(width: 1, color: Colors.black),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'AED 10',
+                        style: TextStyle(
+                            fontSize: 45, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                children: [
+                  Box(
+                    
+                    title: 'AED 10',
+                    ontap: () {
+                      setState(() {
+                        Selectedvalue = 'AED 10';
+                      });
+                    },
+                    selected: Selectedvalue == 'AED 10' ? true : false,
+                  ),
+                  Box(
+                    title: 'AED 20',
+                    ontap: () {
+                      setState(() {
+                        Selectedvalue = 'AED 20';
+                      });
+                    },
+                    selected: Selectedvalue == 'AED 20' ? true : false,
+                  ),
+                  Box(
+                    title: 'AED 50',
+                    ontap: () {
+                      setState(() {
+                        Selectedvalue = 'AED 50';
+                      });
+                    },
+                    selected: Selectedvalue == 'AED 50' ? true : false,
+                  ),
+                  Box(
+                    title: 'AED 100',
+                    ontap: () {
+                      setState(() {
+                        Selectedvalue = 'AED 100';
+                      });
+                    },
+                    selected: Selectedvalue == 'AED 100' ? true : false,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
