a3a615af2b93d3051c504e155eb000fba71eaa09
Adil Awan <tritechuzaifa@gmail.com>
2023-01-28 14:51:39 +0500

    Adil, 28/1/2023

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:klicks_app/screen/home/navigation_screen.dart';
+import 'package:klicks_app/screen/intro/intro.dart';
+import 'package:klicks_app/screen/login/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,6 +14,34 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool authCheck = false;
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      final prefs = await SharedPreferences.getInstance();
+      final String? authCheck = prefs.getString('api_token');
+      if (authCheck == null) {
+        Navigator.of(context).pushReplacement(
+            new MaterialPageRoute(builder: (context) => new LoginScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            new MaterialPageRoute(builder: (context) => new BottomNavScreen()));
+      }
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new IntroScreen()));
+    }
+  }
+
+  Future checkAuth() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? authCheck = prefs.getString('api_token');
+    setState(() {});
+  }
+
   @override
   void initState() {
     super.initState();
@@ -21,7 +53,7 @@ class _SplashScreenState extends State<SplashScreen> {
         const Duration(
           seconds: 3,
         ), () {
-      Navigator.popAndPushNamed(context, 'intro');
+      checkFirstSeen();
     });
   }
 
