2c5d90b8c15fc45fda14e22ad7f9eb620fcc3102
unknown <tritechuzaifa@gmail.com>
2023-01-28 15:13:36 +0500

    ALi

--- lib/api/city.dart
+++ lib/api/city.dart
@@ -2,16 +2,19 @@ import 'package:klicks_app/api/api.dart';
 import 'package:klicks_app/values/string.dart';
 
 class CityApi {
- static getall() async {
-  
-
+  static getall() async {
     var url = BASE_URL + 'city/all';
 
-    var data = {};
+    var response = await Api.execute(url: url);
+
+    print(response);
+  }
+
+  static getmall(id) async {
+    var url = BASE_URL + 'get/mall';
+
+    var data = {'city_id': id};
 
     var response = await Api.execute(url: url, data: data);
-    
-
-   
   }
 }
